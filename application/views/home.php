8edb3008e6ee4f99e9508c1d99fb5aae5510baaf
lilian <lilianwang@sjtu.edu.cn>
2014-11-17 05:04:25 -0500

    Functionality: home view framework

---
+++ application/views/home.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Radiology</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	 <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/sb-admin-2.js"></script>
+	<script type="text/javascript">
+    $(function() {
+// login & sign-up tab
+        $("#get-list").click(function(){
+			$.post("ajax/get_list", 
+			{		
+				user_id:<?php echo $this->session->userdata('user_id');?>
+            },
+			function(data,status){				
+				data = eval("(" + data + ")");				
+				//console.log(data);
+				var innerHTML="<ul>";
+				for (var i=0;i<data.length;i++){
+					innerHTML+="<div class=\"thumbnail\">";
+					innerHTML+="<img src=\""+data[i].thumbimg_dir+"\">";
+					innerHTML+="<div class=\"caption\"><h4>"+data[i].description+"</h4></div>"
+					innerHTML+="<p>"+data[i].dining_time;
+					if (data[i].restaurant_name!="") innerHTML+=" @ "+data[i].restaurant_name;
+					innerHTML+="</p></div>	<hr />";
+				}
+				innerHTML+="</ul>";
+				document.getElementById("list").innerHTML = innerHTML;	
+				//data.metadata.image_path_prefix+data.data.relationships.primary_image.items[0].path
+				//var innerHTML="<a class=\"pull-left\" href=\"#\"> <img src=\""+data.metadata.image_path_prefix+data.data.relationships.primary_image.items[0].path+"\" class=\"media-object\" onclick=\"get_metrics(this)\" width=100px> </a>";			
+				//innerHTML+="<h4 class=\"media-heading\">"+document.getElementById("company_name").value+"</h4>";
+				//document.getElementById("focus").innerHTML += innerHTML;		   
+			});
+			$("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+		});		
+		$("#get-home").click(function(){
+            $("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+        });
+        $("#get-add-protocal").click(function(){
+            $("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+        });
+		$("#get-import").click(function(){
+            $("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+        });
+		$("#get-search").click(function(){
+            $("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+        });
+		$("#get-api").click(function(){
+            $("#home").show();
+			$("#add-protocal").hide();
+			$("#import").hide();
+			$("#search").hide();
+			$("#api").hide();
+        });
+		$("#get-home").trigger("click");
+	});
+	</script>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Radiology</a>
+            </div>
+            <!-- /.navbar-header -->
+			<ul class="nav navbar-top-links navbar-left">   
+												
+							<li><a id="get-home" href="#">Home</a></li>
+							<li><a id="get-add-protocal" href="#">Add Protocal</a></li>
+							<li><a id="get-import" href="#">Import</a></li>							
+							<li><a id="get-search" href="#">Advanced Search</a></li>
+							<li><a id="get-api" href="#">API</a></li>							
+						
+			</ul>
+			
+            <ul class="nav navbar-top-links navbar-right">      
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="/radiology/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+					
+            <div class="navbar-default sidebar" role="navigation">				
+                <div class="sidebar-nav navbar-collapse">					
+                    <ul class="nav" id="side-menu">
+						<li>
+							<h4 class="navbar-brand">Protocals</h4>
+						</li>
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="flot.html">Flot Charts</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html">Morris.js Charts</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="panels-wells.html">Panels and Wells</a>
+                                </li>
+                                <li>
+                                    <a href="buttons.html">Buttons</a>
+                                </li>
+                                <li>
+                                    <a href="notifications.html">Notifications</a>
+                                </li>
+                                <li>
+                                    <a href="typography.html">Typography</a>
+                                </li>
+                                <li>
+                                    <a href="grid.html">Grid</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Third Level <span class="fa arrow"></span></a>
+                                    <ul class="nav nav-third-level">
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                    </ul>
+                                    <!-- /.nav-third-level -->
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li class="active">
+                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="active" href="blank.html">Blank Page</a>
+                                </li>
+                                <li>
+                                    <a href="login.html">Login Page</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <!-- Page Content -->
+        <div id="page-wrapper" name="home">
+			<div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Home</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>            
+            <!-- /.row -->
+			<div class="row">
+                <div class="col-lg-12">
+				Welcome to the radiology.com web site. This is the world’s first site that can convert MR & CT scanning sequence parameters in machine language directly into an easily readable format, and allow you to download a file to import directly into your own scanner without the time consuming practice of entering every sequence manually. Please visit our about page for more information. We are currently adding new protocols to this site in every category.
+				</div>
+			</div>
+        </div>
+        <!-- /#page-wrapper -->
+		
+    </div>
+    <!-- /#wrapper -->
+
+
+</body>
+
+</html>
